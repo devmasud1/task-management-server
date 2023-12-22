f70f6eb95f244d3dba280fa5cfd594c3d3bf27b1
devmasud1 <devmahmud.masu1@gmail.com>
2023-12-22 14:06:17 +0600

    mongoDB setup

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aunb3y8.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const toDoCollection = client.db("toDoDB").collection("toDo");
+
+    app.post("/to-do", async (req, res) => {
+      const toDoInfo = req.body;
+      const result = await reviewsCollection.insertOne(toDoInfo);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ToDo - ToDo is running!");
+});
+
+app.listen(port, () => {
+  console.log(`ToDo  listening on port ${port}`);
+});
